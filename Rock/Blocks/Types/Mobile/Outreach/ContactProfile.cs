c2f7fc075b72390aa6a43fc02b9ff70bd6d348de
Panha-sim <panha.simwestern001@gmail.com>
2025-09-18 14:38:28 -0700

    - Working Condition.

--- Rock/Blocks/Types/Mobile/Outreach/ContactProfile.cs
+++ Rock/Blocks/Types/Mobile/Outreach/ContactProfile.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.ComponentModel;
 
-using OpenXmlPowerTools;
-
 using Rock.Attribute;
 using Rock.Mobile;
 using Rock.Model;
 using Rock.Utility;
+
+using Gender = Rock.Model.Gender;
+using RelationshipFocus = Rock.Enums.Outreach.RelationshipFocus;
+using RelationshipStrength = Rock.Enums.Outreach.RelationshipStrength;
 
 namespace Rock.Blocks.Types.Mobile.Outreach
 {
@@ -73,6 +75,109 @@ namespace Rock.Blocks.Types.Mobile.Outreach
             };
 
             return ActionOk( contactProfile );
+        }
+
+        /// <summary>
+        /// Changes the contact image.
+        /// </summary>
+        /// <param name="contactIdKey"></param>
+        /// <param name="photoGuid"></param>
+        /// <returns></returns>
+        [BlockAction]
+        public BlockActionResult ChangeContactImage( string contactIdKey, Guid photoGuid )
+        {
+            ContactService contactService = new ContactService( RockContext );
+            var contact = contactService.Get( contactIdKey );
+            if ( contact == null )
+            {
+                return ActionBadRequest( "Contact not found." );
+            }
+
+            var binaryFileService = new BinaryFileService( RockContext );
+
+            // If the contact already has a photo, delete it (we don't want to keep old photos lying around).
+            if ( contact.PhotoId.HasValue )
+            {
+                var oldPhoto = binaryFileService.Get( contact.PhotoId.Value );
+                if ( oldPhoto != null )
+                {
+                    binaryFileService.Delete( oldPhoto );
+                }
+            }
+
+            // Get the new photo
+            var newPhoto = binaryFileService.Get( photoGuid );
+            if ( newPhoto == null )
+            {
+                return ActionBadRequest( "There was a problem changing profile picture." );
+            }
+
+            contact.PhotoId = newPhoto.Id;
+            RockContext.SaveChanges();
+
+            return ActionOk();
+        }
+
+        /// <summary>
+        /// Updates the contact.
+        /// </summary>
+        /// <param name="contactIdKey"></param>
+        /// <param name="contactProfileBag"></param>
+        /// <returns></returns>
+        [BlockAction]
+        public BlockActionResult UpdateContact( string contactIdKey, ContactProfileBag contactProfileBag )
+        {
+            ContactService contactService = new ContactService( RockContext );
+            var contact = contactService.Get( contactIdKey );
+            if ( contact == null )
+            {
+                return ActionBadRequest( "Contact not found." );
+            }
+
+            var newRelationshipFocus = ( RelationshipFocus ) contactProfileBag.RelationshipFocus;
+            var newRelationshipStrength = ( RelationshipStrength ) contactProfileBag.RelationshipStrength;
+
+            RockContext.WrapTransaction( () =>
+            {
+                // If the relationship strength has changed, create a new ContactRelationshipStrengthChanges record.
+                if ( contact.RelationshipStrength != newRelationshipStrength )
+                {
+                    ContactRelationshipStrengthChangesService contactRelationshipStrengthChangesService = new ContactRelationshipStrengthChangesService( RockContext );
+                    var contactRelationshipStrengthChange = new ContactRelationshipStrengthChanges
+                    {
+                        ContactId = contact.Id,
+                        PreviousRelationshipStrength = contact.RelationshipStrength,
+                        NewRelationshipStrength = newRelationshipStrength,
+                        AppInfluencedGrowth = false, // PS TODO: How should we determine this?
+                    };
+
+                    contactRelationshipStrengthChangesService.Add( contactRelationshipStrengthChange );
+                }
+
+                contact.FirstName = contactProfileBag.FirstName;
+                contact.LastName = contactProfileBag.LastName;
+                contact.Email = contactProfileBag.Email;
+                contact.MobilePhone = contactProfileBag.MobilePhone;
+                contact.Gender = ( Gender ) contactProfileBag.Gender;
+                contact.BirthDay = contactProfileBag.BirthDay;
+                contact.BirthMonth = contactProfileBag.BirthMonth;
+                contact.BirthYear = contactProfileBag.BirthYear;
+                contact.WeddingAnniversaryDay = contactProfileBag.AnniversaryDay;
+                contact.WeddingAnniversaryMonth = contactProfileBag.AnniversaryMonth;
+                contact.WeddingAnniversaryYear = contactProfileBag.AnniversaryYear;
+                contact.SalvationDay = contactProfileBag.SalvationDay;
+                contact.SalvationMonth = contactProfileBag.SalvationMonth;
+                contact.SalvationYear = contactProfileBag.SalvationYear;
+                contact.BaptismDay = contactProfileBag.BaptismDay;
+                contact.BaptismMonth = contactProfileBag.BaptismMonth;
+                contact.BaptismYear = contactProfileBag.BaptismYear;
+                contact.RelationshipFocus = newRelationshipFocus;
+                contact.RelationshipStrength = newRelationshipStrength;
+
+                RockContext.SaveChanges();
+            } );
+
+            return ActionOk();
         }
 
         #endregion
