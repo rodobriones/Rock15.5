b31ae0f2898b739d7444d68de4517a13da7d6936
SteindelSE <derek.steindel@gmail.com>
2025-10-02 12:45:55 -0700

    - Add template for PersonDuplicateList

---
+++ Rock.Blocks/Crm/PersonDuplicateList.cs
@@ -0,0 +1,141 @@
+﻿// <copyright>
+// Copyright by the Spark Development Network
+//
+// Licensed under the Rock Community License (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.rockrms.com/license
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Entity;
+using System.Linq;
+
+using Rock.Attribute;
+using Rock.Data;
+using Rock.Model;
+using Rock.Obsidian.UI;
+using Rock.Security;
+using Rock.ViewModels.Blocks;
+using Rock.ViewModels.Blocks.Crm.PersonDuplicateList;
+using Rock.Web.Cache;
+
+namespace Rock.Blocks.Crm
+{
+    /// <summary>
+    /// Displays a list of person duplicates.
+    /// </summary>
+
+    [DisplayName( "Person Duplicate List" )]
+    [Category( "CRM" )]
+    [Description( "Displays a list of person duplicates." )]
+    [IconCssClass( "ti ti-list" )]
+    // [SupportedSiteTypes( Model.SiteType.Web )]
+
+    [LinkedPage( "Detail Page",
+        Description = "The page that will show the person duplicate details.",
+        Key = AttributeKey.DetailPage )]
+
+    [Rock.SystemGuid.EntityTypeGuid( "ed3aaaf7-965f-4aef-89e3-961421ce8148" )]
+    [Rock.SystemGuid.BlockTypeGuid( "13B356A9-BCA2-4CFC-A698-86A6C9FFD13B" )]
+    [CustomizedGrid]
+    public class PersonDuplicateList : RockEntityListBlockType<PersonDuplicate>
+    {
+        #region Keys
+
+        private static class AttributeKey
+        {
+            public const string DetailPage = "DetailPage";
+        }
+
+        private static class NavigationUrlKey
+        {
+            public const string DetailPage = "DetailPage";
+        }
+
+        #endregion Keys
+
+        #region Methods
+
+        /// <inheritdoc/>
+        public override object GetObsidianBlockInitialization()
+        {
+            var box = new ListBlockBox<PersonDuplicateListOptionsBag>();
+            var builder = GetGridBuilder();
+
+            box.IsAddEnabled = GetIsAddEnabled();
+            box.IsDeleteEnabled = false;
+            box.ExpectedRowCount = null;
+            box.NavigationUrls = GetBoxNavigationUrls();
+            box.Options = GetBoxOptions();
+            box.GridDefinition = builder.BuildDefinition();
+
+            return box;
+        }
+
+        /// <summary>
+        /// Gets the box options required for the component to render the list.
+        /// </summary>
+        /// <returns>The options that provide additional details to the block.</returns>
+        private PersonDuplicateListOptionsBag GetBoxOptions()
+        {
+            var options = new PersonDuplicateListOptionsBag();
+
+            return options;
+        }
+
+        /// <summary>
+        /// Determines if the add button should be enabled in the grid.
+        /// <summary>
+        /// <returns>A boolean value that indicates if the add button should be enabled.</returns>
+        private bool GetIsAddEnabled()
+        {
+            var entity = new PersonDuplicate();
+
+            return entity.IsAuthorized( Authorization.EDIT, RequestContext.CurrentPerson );
+        }
+
+        /// <summary>
+        /// Gets the box navigation URLs required for the page to operate.
+        /// </summary>
+        /// <returns>A dictionary of key names and URL values.</returns>
+        private Dictionary<string, string> GetBoxNavigationUrls()
+        {
+            return new Dictionary<string, string>
+            {
+                [NavigationUrlKey.DetailPage] = this.GetLinkedPageUrl( AttributeKey.DetailPage, "PersonDuplicateId", "((Key))" )
+            };
+        }
+
+        /// <inheritdoc/>
+        protected override IQueryable<PersonDuplicate> GetListQueryable( RockContext rockContext )
+        {
+            return base.GetListQueryable( rockContext );
+        }
+
+        /// <inheritdoc/>
+        protected override GridBuilder<PersonDuplicate> GetGridBuilder()
+        {
+            return new GridBuilder<PersonDuplicate>()
+                .WithBlock( this )
+                .AddTextField( "idKey", a => a.IdKey )
+                .AddField( "isSecurityDisabled", a => !a.IsAuthorized( Authorization.ADMINISTRATE, RequestContext.CurrentPerson ) )
+                .AddAttributeFields( GetGridAttributes() );
+        }
+
+        #endregion
+
+        #region Block Actions
+
+        #endregion
+    }
+}
