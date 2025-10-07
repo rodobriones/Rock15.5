374ac12e7177269a30b553b95e281a0ee041939f
SteindelSE <derek.steindel@gmail.com>
2025-10-07 15:25:57 -0700

    - Finish backend for PersonDuplicateDetail

--- Rock.Blocks/Crm/PersonDuplicateDetail.cs
+++ Rock.Blocks/Crm/PersonDuplicateDetail.cs
@@ -17,6 +17,7 @@
 
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Data.Entity;
 using System.Linq;
 
 using Rock.Attribute;
@@ -24,11 +25,13 @@ using Rock.Data;
 using Rock.Model;
 using Rock.Obsidian.UI;
 using Rock.Security;
-using Rock.SystemGuid;
 using Rock.Utility.Enums;
 using Rock.ViewModels.Blocks;
-using Rock.ViewModels.Blocks.Crm.PersonDuplicateList;
+using Rock.ViewModels.Blocks.Crm.PersonDuplicateDetail;
+using Rock.ViewModels.Core.Grid;
+using Rock.ViewModels.Utility;
 using Rock.Web.Cache;
+using Rock.Web.UI.Controls;
 
 namespace Rock.Blocks.Crm
 {
@@ -76,7 +79,7 @@ namespace Rock.Blocks.Crm
 
     #endregion Block Attributes
 
-    [Rock.SystemGuid.EntityTypeGuid( "ed3aaaf7-965f-4aef-89e3-961421ce8148" )]
+    [Rock.SystemGuid.EntityTypeGuid( "B96C02DC-F624-4953-BED3-F7BA52CE854D" )]
     [Rock.SystemGuid.BlockTypeGuid( "AAA53F35-1891-4236-B9CB-37805B9134DF" )]
     [CustomizedGrid]
     public class PersonDuplicateDetail : RockEntityListBlockType<PersonDuplicate>
@@ -104,6 +107,11 @@ namespace Rock.Blocks.Crm
             public const string DetailPage = "DetailPage";
         }
 
+        private static class PageParameterKey
+        {
+            public const string PersonId = "PersonId";
+        }
+
         #endregion Keys
 
         #region Methods
@@ -113,7 +121,7 @@ namespace Rock.Blocks.Crm
         /// <inheritdoc/>
         public override object GetObsidianBlockInitialization()
         {
-            var box = new ListBlockBox<PersonDuplicateListOptionsBag>();
+            var box = new ListBlockBox<PersonDuplicateDetailOptionsBag>();
             var builder = GetGridBuilder();
 
             box.IsAddEnabled = false;
@@ -130,9 +138,9 @@ namespace Rock.Blocks.Crm
         /// Gets the box options required for the component to render the list.
         /// </summary>
         /// <returns>The options that provide additional details to the block.</returns>
-        private PersonDuplicateListOptionsBag GetBoxOptions()
+        private PersonDuplicateDetailOptionsBag GetBoxOptions()
         {
-            var options = new PersonDuplicateListOptionsBag();
+            var options = new PersonDuplicateDetailOptionsBag();
 
             options.ConfidenceScoreHigh = GetAttributeValue( AttributeKey.ConfidenceScoreHigh ).AsInteger();
             options.ConfidenceScoreLow = GetAttributeValue( AttributeKey.ConfidenceScoreLow ).AsInteger();
@@ -151,66 +159,48 @@ namespace Rock.Blocks.Crm
         {
             return new Dictionary<string, string>
             {
-                [NavigationUrlKey.DetailPage] = this.GetLinkedPageUrl( AttributeKey.DetailPage, "PersonDuplicateId", "((Key))" )
+                [NavigationUrlKey.DetailPage] = this.GetLinkedPageUrl( AttributeKey.DetailPage, "PersonId", "((Key))" )
             };
         }
 
         /// <inheritdoc/>
-        protected override IQueryable<PersonDuplicate> GetListQueryable(RockContext rockContext)
+        protected override IQueryable<PersonDuplicate> GetListQueryable( RockContext rockContext )
         {
             var recordStatusInactiveId = DefinedValueCache.Get( Rock.SystemGuid.DefinedValue.PERSON_RECORD_STATUS_INACTIVE.AsGuid() ).Id;
             var recordTypeBusinessId = DefinedValueCache.Get( Rock.SystemGuid.DefinedValue.PERSON_RECORD_TYPE_BUSINESS.AsGuid() ).Id;
 
-            var query =  ( IQueryable<PersonDuplicate> ) Rock.Reflection.GetQueryableForEntityType( typeof( PersonDuplicate ), RockContext );
+            var personDuplicateService = new PersonDuplicateService( RockContext );
+            var personService = new PersonService( RockContext );
+            int personId =  Rock.Utility.IdHasher.Instance.GetId( PageParameter( "PersonId" ).ToStringSafe() ).Value;
 
-            // Filter down to non-ignored, non-confirmed duplicates where the main and duplicate Person IDs are not the same IDs
-            query = query.Where( personDuplicate => !personDuplicate.IsConfirmedAsNotDuplicate )
-                .Where( personDuplicate => !personDuplicate.IgnoreUntilScoreChanges )
-                .Where( personDuplicate => personDuplicate.PersonAlias.PersonId != personDuplicate.DuplicatePersonAlias.PersonId );
+            //// Take duplicates that aren't confirmed as NotDuplicate and aren't IgnoreUntilScoreChanges.
+            var query = personDuplicateService.Queryable()
+                .Where( a => a.PersonAlias.PersonId == personId && !a.IsConfirmedAsNotDuplicate && !a.IgnoreUntilScoreChanges );
 
-            // Include inactive records if block property is set
-            if ( GetAttributeValue( AttributeKey.IncludeInactive ).AsBoolean() == false )
+            // Don't include records where both the Person and Duplicate are inactive
+            if ( this.GetAttributeValue( AttributeKey.IncludeInactive ).AsBoolean() == false )
             {
-                query = query.Where( personDuplicate =>
-                    !(
-                        personDuplicate.PersonAlias.Person.RecordStatusValueId == recordStatusInactiveId
-                        && personDuplicate.DuplicatePersonAlias.Person.RecordStatusValueId == recordStatusInactiveId
-                    )
-                );
+                query = query.Where( a => !(
+                    a.PersonAlias.Person.RecordStatusValueId == recordStatusInactiveId
+                    && a.DuplicatePersonAlias.Person.RecordStatusValueId == recordStatusInactiveId
+                ) );
             }
 
-            // Include business records if block property is set
-            if ( GetAttributeValue( AttributeKey.IncludeBusinesses ).AsBoolean() == false )
+            // Don't include records where either the Person or Duplicate is a Business
+            if ( this.GetAttributeValue( AttributeKey.IncludeBusinesses ).AsBoolean() == false )
             {
-                query = query.Where( personDuplicate =>
-                    !(
-                        personDuplicate.PersonAlias.Person.RecordTypeValueId == recordTypeBusinessId
-                        || personDuplicate.DuplicatePersonAlias.Person.RecordTypeValueId == recordTypeBusinessId
-                    )
-                );
+                query = query.Where( a => !(
+                    a.PersonAlias.Person.RecordTypeValueId == recordTypeBusinessId
+                    || a.DuplicatePersonAlias.Person.RecordTypeValueId == recordTypeBusinessId
+                ) );
             }
 
-            // Only show confidence scores that at least meet the Low requirement
+            // Don't include records that don't meet the minimum confidence score
             double? confidenceScoreLow = GetAttributeValue( AttributeKey.ConfidenceScoreLow ).AsDoubleOrNull();
             if ( confidenceScoreLow.HasValue )
             {
-                query = query.Where( a => a.ConfidenceScore > confidenceScoreLow );
+                query = query.Where( a => a.ConfidenceScore >= confidenceScoreLow );
             }
-
-            // Add 1 to the value for key personDuplicate.PersonAlias.PersonId for each duplicate found
-            _matchCounts = query
-                .GroupBy(personDuplicate => personDuplicate.PersonAlias.PersonId)
-                .ToDictionary(
-                    group => group.Key,
-                    group => group.Count()
-                );
-
-            // Select the record with the highest confidence score for each Person
-            query = query
-                .GroupBy(personDuplicate => personDuplicate.PersonAlias.PersonId)
-                .Select(group => group.OrderByDescending(personDuplicate => personDuplicate.ConfidenceScore)
-                .ThenBy( personDuplicate => personDuplicate.Id )
-                .FirstOrDefault());
 
             return query;
         }
@@ -234,31 +224,157 @@ namespace Rock.Blocks.Crm
         /// <inheritdoc/>
         protected override GridBuilder<PersonDuplicate> GetGridBuilder()
         {
+            // The rows will consist of the Person and the Person's duplicates,
+            // so we indicate which is which using the "isDuplicateRow" field
+            // and pass ALL of the data and let the front ent sort it out.
             return new GridBuilder<PersonDuplicate>()
                 .WithBlock( this )
                 .AddTextField( "idKey", a => a.IdKey )
+                .AddField( "personIdKey", a => a.PersonAlias.PersonId != a.DuplicatePersonAlias.PersonId ?
+                                               a.DuplicatePersonAlias.Person.IdKey :
+                                               a.PersonAlias.Person.IdKey )
+                .AddField( "isDuplicateRow", a => a.PersonAlias.PersonId != a.DuplicatePersonAlias.PersonId )
                 .AddField( "confidenceScore", a => a.ConfidenceScore.HasValue ? a.ConfidenceScore.Value : 0 )
-                .AddTextField( "accountProtectionProfile", a => a.PersonAlias.Person.AccountProtectionProfile.ConvertToString() )
-                .AddTextField( "firstName", a => a.PersonAlias.Person.FirstName )
-                .AddTextField( "lastName", a => a.PersonAlias.Person.LastName )
-                .AddTextField( "suffix", a => a.PersonAlias.Person.SuffixValue == null ? "" : a.PersonAlias.Person.SuffixValue.ToString() )
-                .AddTextField( "matchCount", a => _matchCounts[a.PersonAlias.PersonId].ToString() )
-                .AddTextField( "modified", a => a.ModifiedDateTime.ToString() )
-                .AddTextField( "createdBy", a => a.CreatedByPersonName )
-                .AddTextField( "campus", a => a.PersonAlias.Person.PrimaryCampus.ToString() )
-                .AddField( "isSecurityDisabled", a => !a.IsAuthorized( Authorization.ADMINISTRATE, RequestContext.CurrentPerson ) )
+                .AddTextField( "campus", a => a.PersonAlias.PersonId != a.DuplicatePersonAlias.PersonId ?
+                                              a.DuplicatePersonAlias.Person.PrimaryCampus.ToStringSafe() :
+                                              a.PersonAlias.Person.PrimaryCampus.ToString() )
+                .AddTextField( "accountProtectionProfile", a => a.PersonAlias.PersonId != a.DuplicatePersonAlias.PersonId ?
+                                                                a.DuplicatePersonAlias.Person.AccountProtectionProfile.ConvertToString() :
+                                                                a.PersonAlias.Person.AccountProtectionProfile.ConvertToString() )
+                .AddTextField( "firstName", a => a.PersonAlias.PersonId != a.DuplicatePersonAlias.PersonId ?
+                                                 a.DuplicatePersonAlias.Person.FirstName :
+                                                 a.PersonAlias.Person.FirstName )
+                .AddTextField( "lastName", a => a.PersonAlias.PersonId != a.DuplicatePersonAlias.PersonId ?
+                                                 a.DuplicatePersonAlias.Person.LastName :
+                                                 a.PersonAlias.Person.LastName )
+                .AddTextField( "email", a => a.PersonAlias.PersonId != a.DuplicatePersonAlias.PersonId ?
+                                             ( string.IsNullOrEmpty( a.DuplicatePersonAlias.Person.Email ) ? "" : a.DuplicatePersonAlias.Person.Email ) :
+                                             ( string.IsNullOrEmpty( a.PersonAlias.Person.Email ) ? "" : a.PersonAlias.Person.Email ) )
+                .AddTextField( "gender", a => a.PersonAlias.PersonId != a.DuplicatePersonAlias.PersonId ?
+                                             ( string.IsNullOrEmpty( a.DuplicatePersonAlias.Person.Gender.ConvertToStringSafe() ) ? "" : a.DuplicatePersonAlias.Person.Gender.ConvertToStringSafe() ) :
+                                             ( string.IsNullOrEmpty( a.PersonAlias.Person.Gender.ConvertToStringSafe() ) ? "" : a.PersonAlias.Person.Gender.ConvertToStringSafe() ) )
+                .AddTextField( "age", a => a.PersonAlias.PersonId != a.DuplicatePersonAlias.PersonId ?
+                                           a.DuplicatePersonAlias.Person.Age.ToStringSafe() :
+                                           a.PersonAlias.Person.Age.ToStringSafe() )
+                .AddField( "addresses", a => a.PersonAlias.PersonId != a.DuplicatePersonAlias.PersonId ?
+                                             GetAddressesWithType( a.DuplicatePersonAlias.Person ) :
+                                             GetAddressesWithType( a.PersonAlias.Person ) )
+                .AddField( "phoneNumbers", a => a.PersonAlias.PersonId != a.DuplicatePersonAlias.PersonId ?
+                                             GetPhoneNumbersWithType( a.DuplicatePersonAlias.Person ) :
+                                             GetPhoneNumbersWithType( a.PersonAlias.Person ) )
                 .AddAttributeFields( GetGridAttributes() );
         }
 
-        #endregion Initialization Methods
+        /// <summary>
+        /// Gets a list of addresses for the given person, along with the type of each address.
+        /// </summary>
+        /// <param name="person">The person whose addresses will be retrieved.</param>
+        /// <returns>
+        /// A list of key-value pairs where the key is the <see cref="Location"/> and the value is the address type as a <see cref="string"/>.
+        /// </returns>
+        public List<KeyValuePair<Location, string>> GetAddressesWithType( Person person )
+        {
+            var addressList = person.GetFamilies()
+                .SelectMany( family => family.GroupLocations )
+                .OrderByDescending( gl => gl.IsMappedLocation )
+                .ThenBy( gl => gl.Id )
+                .Select( gl => new
+                {
+                    Location = gl.Location,
+                    TypeValue = gl.GroupLocationTypeValue != null ? gl.GroupLocationTypeValue.Value : null
+                } )
+                .ToList()
+                .Select( x => new KeyValuePair<Location, string>( x.Location, x.TypeValue ) ) // Selects from in-memory list
+                .ToList();
 
-        #region Helper Methods
+            return addressList;
+        }
+
+        /// <summary>
+        /// Gets a list of phone numbers for the given person, along with the type of each phone number.
+        /// </summary>
+        /// <param name="person">The person whose phone numbers will be retrieved.</param>
+        /// <returns>
+        /// A list of key-value pairs where the key is the <see cref="PhoneNumber"/> and the value is the phone number type as a <see cref="string"/>.
+        /// </returns>
+        public List<KeyValuePair<PhoneNumber, string>> GetPhoneNumbersWithType(Person person)
+        {
+            if (person.PhoneNumbers == null)
+            {
+                return new List<KeyValuePair<PhoneNumber, string>>();
+            }
+
+            var phoneNumberList = person.PhoneNumbers
+                .OrderBy(pn => pn.NumberTypeValue != null ? pn.NumberTypeValue.Order : int.MaxValue)
+                .ThenBy(pn => pn.Id)
+                .ToList()
+                .Select(pn => new KeyValuePair<PhoneNumber, string>(pn, pn.NumberTypeValue != null ? pn.NumberTypeValue.Value : null))
+                .ToList();
+
+            return phoneNumberList;
+        }
 
         #endregion Helper Methods
 
         #endregion Methods
 
         #region Block Actions
+
+        /// <summary>
+        /// Gets the bag that describes the grid data to be displayed in the
+        /// block.
+        /// </summary>
+        /// <returns>An action result that contains the grid data.</returns>
+        [BlockAction]
+        public virtual BlockActionResult GetPerson()
+        {
+            var gridDataBag = new GridDataBag();
+
+            var personIdParam = PageParameter(PageParameterKey.PersonId).ToStringSafe();
+            var personId = Rock.Utility.IdHasher.Instance.GetId(personIdParam);
+            if (!personId.HasValue)
+            {
+                return ActionNotFound();
+            }
+
+            var personService = new PersonService(RockContext);
+            var person = personService.Get(personId.Value);
+            if (person == null)
+            {
+                return ActionNotFound();
+            }
+
+            var row = new Dictionary<string, object>
+            {
+                ["idKey"] = person.IdKey,
+                ["personIdKey"] = person.IdKey,
+                ["isDuplicateRow"] = false,
+                ["confidenceScore"] = 0,
+                ["campus"] = person.PrimaryCampus != null ? person.PrimaryCampus.ToStringSafe() : string.Empty,
+                ["accountProtectionProfile"] = person.AccountProtectionProfile.ConvertToString(),
+                ["firstName"] = person.FirstName,
+                ["lastName"] = person.LastName,
+                ["email"] = string.IsNullOrEmpty(person.Email) ? "" : person.Email,
+                ["gender"] = string.IsNullOrEmpty(person.Gender.ConvertToStringSafe()) ? "" : person.Gender.ConvertToStringSafe(),
+                ["age"] = person.Age.ToStringSafe(),
+                ["addresses"] = GetAddressesWithType(person),
+                ["phoneNumbers"] = GetPhoneNumbersWithType(person)
+            };
+
+            var gridAttributes = GetGridAttributes();
+            if (gridAttributes != null && gridAttributes.Any())
+            {
+                person.LoadAttributes(RockContext);
+                foreach (var attr in gridAttributes)
+                {
+                    row[attr.Key] = person.GetAttributeValue(attr.Key);
+                }
+            }
+
+            gridDataBag.Rows = new List<Dictionary<string, object>> { row };
+
+            return ActionOk(gridDataBag);
+        }
 
         #endregion
     }
