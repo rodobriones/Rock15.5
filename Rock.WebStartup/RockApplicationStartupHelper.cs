832a4c1feeca652f5a119c8971f5c20ba0a16090
kylehenning <kylebhenning@gmail.com>
2025-10-22 17:05:08 -0700

    - Moved one-time SQL script in startup to an EF Migration.

--- Rock.WebStartup/RockApplicationStartupHelper.cs
+++ Rock.WebStartup/RockApplicationStartupHelper.cs
@@ -137,8 +137,6 @@ namespace Rock.WebStartup
 
             ShowDebugTimingMessage( "EF Migrations" );
 
-            TempOneTimeDataMigration();
-
             // Register Entity SaveHooks.
             LogStartupMessage( "Configuring Entity SaveHooks" );
             ConfigureEntitySaveHooks();
@@ -636,52 +634,6 @@ namespace Rock.WebStartup
             }
 
             return false;
-        }
-
-        private static void TempOneTimeDataMigration()
-        {
-            var sql = @"
-IF EXISTS (SELECT 1 FROM dbo.[Person] WHERE [Guid] = 'ad28da19-4af1-408f-9090-2672f8376f27')
-BEGIN
-
-    BEGIN TRANSACTION
-
-    DECLARE @NewPersonGuid uniqueidentifier = NEWID()
-    DECLARE @NewPersonAliasGuid uniqueidentifier = NEWID()
-
-    UPDATE [Person]
-    SET [Guid] = @NewPersonGuid
-    WHERE [Guid] = 'ad28da19-4af1-408f-9090-2672f8376f27'
-
-    DECLARE @PersonFieldTypeId int = (SELECT TOP 1 [Id] FROM [FieldType] WHERE [Guid] = 'E4EAB7B2-0B76-429B-AFE4-AD86D7428C70')
-
-    -- Update built-in admin user person alias guid (if it still exists)
-    UPDATE [PersonAlias]
-    SET [Guid] = @NewPersonAliasGuid
-        ,[AliasPersonGuid] = @NewPersonGuid
-    WHERE [Guid] = '996c8b72-c255-40e6-bb98-b1d5cf345f3b' AND [AliasPersonGuid] = 'ad28da19-4af1-408f-9090-2672f8376f27'
-
-    -- Update the old PersonAlias.[AliasPersonGuid] with their new @NewPersonGuid
-    UPDATE [PersonAlias]
-    SET [AliasPersonGuid] = @NewPersonGuid
-    WHERE [PersonId] = 1 AND [AliasPersonGuid] = 'ad28da19-4af1-408f-9090-2672f8376f27'
-
-    -- Update the Person with the new top primary alias  (NOT AVAILABLE IN v16.x)
-    UPDATE [Person]
-    SET [PrimaryAliasGuid] = (SELECT TOP 1 [Guid] FROM [PersonAlias] WHERE Id = Person.PrimaryAliasId)
-    WHERE [Guid] = @NewPersonGuid
-
-    -- Update attribute values that reference the old person alias guid
-    UPDATE av
-        SET av.[Value] = CAST(@NewPersonAliasGuid AS nvarchar(36))
-    FROM [AttributeValue] av
-    INNER JOIN [Attribute] a ON av.[AttributeId] = a.[Id]
-    WHERE a.[FieldTypeId] = @PersonFieldTypeId AND av.[ValueChecksum] = '817829624' AND av.[Value] = '996c8b72-c255-40e6-bb98-b1d5cf345f3b'
-
-    COMMIT TRANSACTION
-END
-";
-            DbService.ExecuteCommand( sql, System.Data.CommandType.Text, null, 60 );
         }
 
         /// <summary>
