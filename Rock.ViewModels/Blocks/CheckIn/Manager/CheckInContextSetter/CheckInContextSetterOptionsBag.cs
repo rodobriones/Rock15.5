9da77684a754bde56bf703e7f8b698a8ac4d6a97
Daniel Hazelbaker <daniel@triumph.tech>
2025-12-02 14:35:29 -0700

    - Added Check-in Context Setter block.

---
+++ Rock.ViewModels/Blocks/CheckIn/Manager/CheckInContextSetter/CheckInContextSetterOptionsBag.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Rock.ViewModels.Utility;
+
+namespace Rock.ViewModels.Blocks.CheckIn.Manager.CheckInContextSetter
+{
+    /// <summary>
+    /// The initialization options for the Check-in Context Setter block.
+    /// </summary>
+    public class CheckInContextSetterOptionsBag
+    {
+        /// <summary>
+        /// The list of campuses to display in the dropdown.
+        /// </summary>
+        public List<ListItemBag> Campuses { get; set; }
+
+        /// <summary>
+        /// The root locations to use for the location picker.
+        /// </summary>
+        public Dictionary<Guid, Guid> RootLocations { get; set; }
+
+        /// <summary>
+        /// The selected campus when the block loaded.
+        /// </summary>
+        public ListItemBag SelectedCampus { get; set; }
+
+        /// <summary>
+        /// The selected location when the block loaded.
+        /// </summary>
+        public ListItemBag SelectedLocation { get; set; }
+
+        /// <summary>
+        /// The selected schedule when the block loaded.
+        /// </summary>
+        public ListItemBag SelectedSchedule { get; set; }
+    }
+}
